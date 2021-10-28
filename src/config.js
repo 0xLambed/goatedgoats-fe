b9f2581a0f4d16380ac0107195112849e152f342
aMemoryLeak <4memory.leak@gmail.com>
2021-10-27 22:06:18 -0700

    Added .env variables for contracts in testnet

--- src/config.js
+++ src/config.js
@@ -6,3 +6,5 @@ config()
   .put("accessNode.api", process.env.REACT_APP_ACCESS_NODE) // Configure FCL's Access Node
   .put("discovery.wallet", process.env.REACT_APP_WALLET_DISCOVERY) // Configure FCL's Wallet Discovery mechanism
   .put("0xProfile", process.env.REACT_APP_CONTRACT_PROFILE) // Will let us use `0xProfile` in our Cadence
+  .put("0xVoucher", process.env.REACT_APP_TESTNET_CONTRACT_VOUCHER)
+  .put("0xCollectible", process.env.REACT_APP_TESTNET_CONTRACT_COLLECTIBLE)
